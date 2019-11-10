6f0181ce332a83b912835e6353da213a8269ff51
Johnson Fu <johnson.fu@gmail.com>
2019-11-11 00:15:31 +0800

    export and import

--- 037-js-modules.js
+++ 037-js-modules.js
@@ -41,4 +41,57 @@ var fightModule = (function() {
 // the global scope variables will not be changed.
 //
 
+// CommonJS
+
+function fight() {
+
+}
+
+module.exports = {
+    fight: fight
+}
+ 
+// ----
+// in js file
+export function fight(x,y) {
+    return x > y;
+}
+
+export default function abc() {
+    return "xyz"
+}
+
+// --- in html
+// import {fight} from "fight.js"
+
+
+// named exports
+
+//------ lib.js ------
+export const sqrt = Math.sqrt;
+export function square(x) {
+    return x * x;
+}
+export function diag(x, y) {
+    return sqrt(square(x) + square(y));
+}
+
+export {sqrt, square, diag}
+
+//------ main.js ------
+import { square, diag } from 'lib';				
+console.log(square(11)); // 121
+console.log(diag(4, 3)); // 5
+
+// aliasing named imports
+
+import {square as sq} from 'lib';
+
+//  namedspace import
+
+import * as lib1 from './lib';
+
+lib1.square(1);
+
+
 
